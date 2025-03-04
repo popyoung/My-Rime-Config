57bb06f786e2ca0795b62e028b223d56ab334c63
popyoung <popyoung@163.com>
2025-03-04 17:43:48 +0800

    补充遗失文件

--- UserFolder/重建UserFolder符号链接.bat
+++ UserFolder/重建UserFolder符号链接.bat
@@ -6,6 +6,7 @@ del "C:\Users\popyoung\AppData\Roaming\Rime\lua\EnglishSymbols.lua"
 del "C:\Users\popyoung\AppData\Roaming\Rime\lua\json.lua"
 del "C:\Users\popyoung\AppData\Roaming\Rime\lua\trigger.lua"
 del "C:\Users\popyoung\AppData\Roaming\Rime\lua\is_in_user_dict.lua"
+del "C:\Users\popyoung\AppData\Roaming\Rime\lua\single_char_mode.lua"
 mklink "C:\Users\popyoung\AppData\Roaming\Rime\rime.lua"  "%CD%\rime.lua"
 mklink "C:\Users\popyoung\AppData\Roaming\Rime\rime_frost.custom.yaml" "%CD%\rime_frost.custom.yaml"
 mklink "C:\Users\popyoung\AppData\Roaming\Rime\lua\baidu.lua" "%CD%\lua\baidu.lua"
@@ -14,4 +15,5 @@ mklink "C:\Users\popyoung\AppData\Roaming\Rime\lua\EnglishSymbols.lua" "%CD%\lua
 mklink "C:\Users\popyoung\AppData\Roaming\Rime\lua\json.lua" "%CD%\lua\json.lua"
 mklink "C:\Users\popyoung\AppData\Roaming\Rime\lua\trigger.lua" "%CD%\lua\trigger.lua
 mklink "C:\Users\popyoung\AppData\Roaming\Rime\lua\is_in_user_dict.lua" "%CD%\lua\is_in_user_dict.lua
+mklink "C:\Users\popyoung\AppData\Roaming\Rime\lua\single_char_mode.lua" "%CD%\lua\single_char_mode.lua
 pause
